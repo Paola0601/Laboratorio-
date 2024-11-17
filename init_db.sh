69cd2e80bfd577a1ef8fdba217bc9bc4b064392f
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-11-16 22:46:17 -0500

    decimo intento

--- init_db.sh
+++ init_db.sh
@@ -11,9 +11,5 @@ mysql -u root -e "USE prueba;
         FOREIGN KEY (pelicula_id) REFERENCES peliculas(pelicula_id) ON DELETE CASCADE,
         FOREIGN KEY (actor_id) REFERENCES actores(actor_id) ON DELETE CASCADE);"
 
-mysql -u root -e "USE prueba;
-    INSERT INTO actores (nombre) VALUES ('Robert Downey Jr.'), ('Scarlett Johansson'), ('Chris Hemsworth');
-    INSERT INTO peliculas (nombre, year, vote, score) VALUES ('Avengers: Endgame', 2019, 8500, 8.4), ('Iron Man', 2008, 4000, 7.9), ('Thor', 2011, 3200, 7.0);
-    INSERT INTO casting (pelicula_id, actor_id, papel) VALUES (1, 1, 'Iron Man'), (1, 2, 'Black Widow'), (1, 3, 'Thor'), (2, 1, 'Iron Man'), (3, 3, 'Thor');"
 
 mysqladmin -u root shutdown
