9ab84ab763f7e04de4507b2c1f8552d3f98ba885
Paola0601 <pmaytaqu@unsa.edu.pe>
2024-11-22 01:30:30 -0500

    modificacion del archivo ejercicio2

--- cgi-bin/ejercicio2.pl
+++ cgi-bin/ejercicio2.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
+use CGI;
 use DBI;
+
+my $cgi = CGI->new;
+print $cgi->header('text/html; charset=UTF-8');
 
 # Configuración de conexión
 my $database = "prueba";
 my $hostname = "mariadb2";
-my $port     = 3306; 
+my $port     = 3306;
 my $username = "paola";
-my $password = "soyunarata";
+my $password = "adamari";
 
 # DSN de conexión
 my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
@@ -16,7 +20,7 @@ my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
 # Conectar a la base de datos
 my $dbh = DBI->connect($dsn, $username, $password, { RaiseError => 1, PrintError => 0, mysql_enable_utf8 => 1 });
 
-# Consulta ID 5 
+# Consulta ID 5
 my $sql = q{
     SELECT nombre
     FROM actores
@@ -29,18 +33,30 @@ $sth->execute();
 # Obtener el resultado
 my $actor = $sth->fetchrow_array;
 
-# HTML
-print "Content-type: text/html\n\n";
 print <<HTML;
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../estilos.css">
+    <link rel="icon" type="image/png" href="../imagenes/unsa-logo.png">
     <title>Actor ID-5</title>
 </head>
 <body>
-    <h1>ES: </h1>
+    <header>
+        <h1>¡Bienvenido a la base de datos!</h1>
+    </header>
+    <div class="container">
+        <div class="sidebar">
+            <nav>
+                <a href="./ejercicio2.pl" class="nav-link">Actor de ID 5</a>
+                <a href="./ejercicio3.pl" class="nav-link">Actores con ID >= 8</a>
+                <a href="./ejercicio4.pl" class="nav-link">Películas con puntaje mayor a 7 y más de 5000 votos</a>
+            </nav>
+        </div>
+        <div class="main-content">
+            <h2>Resultados de la consulta</h2>
 HTML
 
 if ($actor) {
@@ -50,6 +66,9 @@ if ($actor) {
 }
 
 print <<HTML;
+            <a href="../index.html" class="volver">Volver al índice</a>
+        </div>
+    </div>
 </body>
 </html>
 HTML
